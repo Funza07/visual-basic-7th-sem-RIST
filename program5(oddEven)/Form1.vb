9833ba51f90c88d064637639390b2329f43b9fd5
BIKRAM TRIPATHI <102977156+Funza07@users.noreply.github.com>
2024-11-19 20:13:46 +0530

    Add files via upload

---
+++ program5(oddEven)/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n As Integer
+        n = TextBox1.Text
+        If n Mod 2 = 0 Then
+            Label4.Text = "Even Number"
+        Else
+            Label4.Text = "Odd Number"
+        End If
+    End Sub
+End Class
---
+++ program8(timer)/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+        Label2.Text = TimeOfDay
+    End Sub
+End Class
