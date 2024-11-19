01fac3b903bcc70597da4711f2c2d5309d72c0a2
BIKRAM TRIPATHI <102977156+Funza07@users.noreply.github.com>
2024-11-19 20:10:59 +0530

    Add files via upload

---
+++ program2(factorial)/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n, fact As Integer
+        n = TextBox1.Text
+        fact = 1
+        For i = 1 To n
+            fact *= i
+        Next
+        Label4.Text = fact
+    End Sub
+End Class
